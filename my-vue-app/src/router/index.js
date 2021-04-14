90b1ff05f4860785b8cb7c8be94aefd97ec6b8dd
Spencer <sjones97@email.unc.edu>
2021-04-14 12:44:55 -0400

    initial frontend setup

--- my-vue-app/src/router/index.js
+++ my-vue-app/src/router/index.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import PostsManager from '@/components/PostsManager'
+import OktaVue, { LoginCallback } from '@okta/okta-vue'
+import { OktaAuth } from '@okta/okta-auth-js'
+
+const oktaAuth = new OktaAuth({
+  issuer: 'https://dev-10307142-admin.okta.com/oauth2/default',
+  clientId: '0oal3g1qmkVJR90fb5d6',
+  redirectUri: window.location.origin + '/callback',
+  scopes: ['openid', 'profile', 'email']
+})
 
 Vue.use(Router)
+Vue.use(OktaVue, { oktaAuth })
 
 export default new Router({
   routes: [
@@ -10,6 +21,18 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
-    }
+    },
+     {
+       path: '/callback',
+       component: LoginCallback
+     },
+     {
+       path: '/posts-manager',
+       name: 'PostsManager',
+       component: PostsManager,
+       meta: {
+         requiresAuth: true
+       }
+     }
   ]
 })
